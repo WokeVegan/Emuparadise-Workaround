a17c7a0ec15127a35cbe4adc37f3ae93792dd7b5
Lopwel <nrpg97@gmail.com>
2019-06-04 05:59:19 -0400

    - added detailed progress bar

--- emu-dl.py
+++ emu-dl.py
@@ -4,6 +4,7 @@ import os
 import argparse
 import requests
 import urllib.parse
+import time
 
 
 def search():
@@ -24,6 +25,16 @@ def search():
         print(x)
 
 
+def get_size_label(total):
+    if total >= 1000000000:
+        return "%.2fGB" % (int(total) / 1000000000)
+    if total >= 1000000:
+        return "%.2fMB" % (int(total) / 1000000)
+    elif total >= 1000:
+        return "%.2fdKB" % (int(total) / 1000)
+    return "%dB" % total
+
+
 def download():
     """ downloads specified url """
     gid = args.download.split('/')[-1]
@@ -31,21 +42,35 @@ def download():
     response = requests.get(game_link, headers={"referer": game_link}, stream=True)
     decoded_url = urllib.parse.unquote(response.url)
     filename = decoded_url.split('/')[-1]
+    install = True
 
     if os.path.exists(filename):
-        print("%s already exists" % filename)
-    elif not os.path.exists(filename):
+        overwrite = input("'%s' already exists.\nOverwrite the file? [y/n] " % os.path.abspath(filename))
+        if overwrite.lower() != 'y':
+            install = False
+
+    if install:
+        print("downloading '%s'" % filename)
+        start_time = time.time()
         total_size = int(response.headers.get('content-length'))
         current_size = 0
+        bar_width = 30
 
-        with open(filename, 'wb') as f:
+        with open(os.path.join(filename), 'wb') as f:
             for block in response.iter_content(2097152):
                 f.write(block)
                 current_size += len(block)
-                percent = '{0:3d}%'.format(int(current_size / total_size * 100))
-                print("\r%3dMB / %3dMB\t%s" % (current_size >> 20, total_size >> 20, percent), end="")
+                download_percent = current_size / total_size * 100
+                bar_percent = "=" * int(current_size / total_size * bar_width)
+                bar_difference = " " * (bar_width - int(current_size / total_size * bar_width))
+                time_label = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
+                size_label = get_size_label(total_size)
+                remaining_time = int(((time.time() - start_time) / download_percent) * (100 - download_percent))
+                time_estimate = time.strftime("%H:%M:%S", time.gmtime(remaining_time))
+                print("\r%s %03d%%[%s%s] %s ETA %s" % (time_label, int(download_percent), bar_percent, bar_difference, size_label, time_estimate), end="")
             f.close()
-        print("\nfile saved to '%s'" % filename)
+
+        print("\nfile saved to '%s'" % os.path.abspath(filename))
 
 
 if __name__ == '__main__':
