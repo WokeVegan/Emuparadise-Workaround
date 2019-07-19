cefffc3e063d2067e3bd1a15162c1a8a1e36fffe
Woke Vegan <29270662+WokeVegan@users.noreply.github.com>
2019-07-19 17:47:56 -0400

    update for platform specific directories

--- src/path.py
+++ src/path.py
@@ -1,15 +1,38 @@
 import os
 import configparser
 
+
 _DIRECTORY_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..",  "settings.cfg")
 
 
-def set_default_directory(directory):
-    """ changes the default download directory. """
+def get_list_platforms():
+    database_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "database")
+    platforms = []
+    for platform in os.listdir(database_path):
+        platforms.append(os.path.splitext(platform)[0])
+    return platforms
+
+
+def create_settings_template():
     config = configparser.ConfigParser()
-    if not config.has_section('DIRECTORY'):
-        config.add_section('DIRECTORY')
-    config['DIRECTORY']['DEFAULT'] = directory
+    config.add_section('DIRECTORY')
+    for platform in get_list_platforms():
+        config['DIRECTORY']['DEFAULT'] = str()
+        config['DIRECTORY'][str(platform)] = str()
+    with open(_DIRECTORY_PATH, 'w', encoding='utf-8') as f:
+        config.write(f)
+    f.close()
+
+
+def set_default_directory(directory, platform=None):
+    """ changes the default download directory. """
+    if not os.path.exists(_DIRECTORY_PATH):
+        create_settings_template()
+    config = configparser.ConfigParser()
+    config.read(_DIRECTORY_PATH)
+    if platform is None:
+        platform = 'default'
+    config['DIRECTORY'][platform] = directory
     with open(_DIRECTORY_PATH, 'w', encoding='utf-8') as f:
         config.write(f)
     f.close()
@@ -17,10 +40,17 @@ def set_default_directory(directory):
     print(f"The default download directory has been set to '{directory}'.")
 
 
-def get_default_directory():
+def get_default_directory(platform=None):
     """ returns the default download directory. """
-    if os.path.isfile(_DIRECTORY_PATH):
+    if not os.path.exists(_DIRECTORY_PATH):
+        create_settings_template()
+    elif os.path.isfile(_DIRECTORY_PATH):
         config = configparser.ConfigParser()
         config.read(_DIRECTORY_PATH, encoding='utf-8')
-        return config.get('DIRECTORY', 'default')
+        if platform is None:
+            platform = 'default'
+        if config.get('DIRECTORY', platform):
+            return config.get('DIRECTORY', platform)
+        elif config.get('DIRECTORY', 'default'):
+            return config.get('DIRECTORY', 'default')
     return os.getcwd()
